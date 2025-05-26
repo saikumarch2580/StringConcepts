13cdf809db15e0133e106ce21bcb269535fa8470
sairam <saikumarch2580@gmail.com>
2025-05-26 17:14:05 +0530

    Modified

--- TestOne/src/test/java/Selenium/TestOne/PracticeOne.java
+++ TestOne/src/test/java/Selenium/TestOne/PracticeOne.java
@@ -8,20 +8,27 @@ import org.testng.annotations.Test;
 
 public class PracticeOne extends BaseClass{
 	
-	WebDriver driver;
-	@Test
+
+ 
+
+	 WebDriver driver;
+	 
+	
+	@Test(priority =1)
 	public void login() throws Exception {
 		
 		 driver = new ChromeDriver();
 		 driver.get("https://app.autorox.co:9443/axuat/");
+		 //driver.get("https://app.autorox.co");
 		 driver.manage().window().maximize();
 		 login(driver);
-		 ClickMenu(driver);		
+		 ClickOnMenuButton(driver);
+		 OpenInward(driver);		
 		 CompareTitle(driver, "Direct Inward - Autorox");
 		 CheckboxCheckandUncheck(driver); 
 		 VendorDetailsEntry(driver);
 		 EnterBillNo(driver);
-		 SelectCalender(driver);
+		 SelectCalender(driver); 
 		 excelinward(driver);
 		 partNosEnter(driver);
 		 Thread.sleep(2000); 
@@ -30,15 +37,37 @@ public class PracticeOne extends BaseClass{
 		 clicksubmittButton(driver);
 		 Thread.sleep(2000);
 		 checkThePaymentsTabIsDisplayedorNot(driver); 
-		 CheckStockDetails(driver);
-		 NavigateToSellProducts(driver);
+		 CheckStockDetails(driver); 
+     	 NavigateToSellProducts(driver);
 		 sellProductsButtonclick(driver);
-		 Thread.sleep(2000);
-		 
-		 
-		  
-		 
+		 Thread.sleep(2000); 
+		 selectSellProductParts(driver);
+		 Click_NextButton(driver);
+		 EnterSellProductsCustomerDetails(driver);
+		 TheFinalNextButton(driver);
+		 CheckTheConfirmationpopupIsdisplayingOrNot(driver);
+		 CreateInvoiceButtonIsDisplayingOrNot(driver);
+		 CreateInvoiceButton(driver);
+	 
+	}
+	@Test(priority =2)
+	public void CancelInvoiceVisible() {
+		
+		CancelInvoiceVisableOrNot(driver);
 	}
 
-} 
+	@Test(priority =3, dependsOnMethods= {"CancelInvoiceVisible"})
+	public void inwardCheckAfterCreatingTheInvoice() throws Exception {
+		
+		checkThePartsareEditableAfterInvoice(driver);
+		inwardCheckAfterCreatingTheInvoice(driver);
+		
+		
+	}
+	
+}  
+		 
+	
+
+
 	 
